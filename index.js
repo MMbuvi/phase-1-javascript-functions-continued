3d616630fc372dacbe5a871839206da48c715516
Meshack <mbuvi.meshack99@gmail.com>
2022-08-22 14:40:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(duty = 'go to the office'){
+    return `This Monday, I will ${ duty}.`
+}
+function wrapAdjective(style = "||"){
+    return function (string = " a good student"){
+    return `You are ${style}${string}${style}!`
+}
+}
